e5c8912c67a590d37730cfc21ea6e4b3c2530fe0
Ivan Fila <opr103osos@gmail.com>
2025-02-11 15:07:50 +0000

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -2336,6 +2336,8 @@ local function FartHubLoad()
 			end,
 		})
 
+		AnimationsTab:CreateSection("You can emote as killer using this.")
+
 		local AnimationsTabKeybindGUI = AnimationsTab:CreateKeybind({
 			Name = "Emote As Killer GUI",
 			CurrentKeybind = "F",
