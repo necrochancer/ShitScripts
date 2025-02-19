8823014cc4a49f7201c02b0a8f1a99a8028704a6
ivannetta <opr103osos@gmail.com>
2025-02-19 22:02:14 +0000

    a

--- PathfindGens.lua
+++ PathfindGens.lua
@@ -4,7 +4,7 @@ local Players = game:GetService("Players")
 
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 if queueteleport then
-	queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ivannetta/ShitScripts/main/BetaPathfindGenerator.lua'))()")
+	queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ivannetta/ShitScripts/main/PathfindGens.lua'))()")
 end
 
 if _G.CancelPathEvent then
