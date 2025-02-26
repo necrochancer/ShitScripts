8997b473b907603c995079f9a30916c9ad53e5fb
ivannetta <opr103osos@gmail.com>
2025-02-26 15:00:21 +0000

    okay

--- PathfindGensCCC.lua
+++ PathfindGensCCC.lua
@@ -5,12 +5,20 @@ local PathfindingService = game:GetService("PathfindingService")
 local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 local DCWebhook = (getgenv and getgenv().DiscordWebhook) or false
-if DCWebhook == "" then DCWebhook = false end
+if DCWebhook == "" then
+	DCWebhook = false
+end
 local ProfilePicture = ""
-local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
-if queueteleport then queueteleport("if getgenv then getgenv().DiscordWebhook = " .. tostring(DCWebhook) .. " end " .. "loadstring(game:HttpGet('https://raw.githubusercontent.com/ivannetta/ShitScripts/main/PathfindGensCCC.lua'))()") end
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+if queueteleport then
+	queueteleport([[
+        if getgenv then getgenv().DiscordWebhook = "]] .. tostring(DCWebhook) .. [[" end
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/ivannetta/ShitScripts/main/PathfindGensCCC.lua'))()
+    ]])
+end
 
 print(DCWebhook)
+
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
 	Title = "PathfindGens",
