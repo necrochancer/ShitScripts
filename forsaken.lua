33d7359ba1972fdaeb80cddef99aa0e9af8b6d54
necrochancer <85408331+necrochancer@users.noreply.github.com>
2025-02-28 21:19:53 -0500

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -3416,20 +3416,23 @@ local function FartHubLoad()
 			end,
 		})
 		local function FemboyElliot()
+    local notificationSent = false -- Flag to track if the notification was sent
+
     while true do
         if player.Character and player.Character.Name == "Elliot" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/necrochancer/ShitScripts/main/FemboyElliot.lua"))()
-        else
+            notificationSent = false -- Reset the flag if the correct character is found.
+        elseif not notificationSent then
             Rayfield:Notify({
                 Title = "Wrong Character",
                 Content = "Oops, your current character isn't Elliot, this POSSIBLY can bug out, so untoggle unless you're on Elliot!",
                 Duration = 5,
             })
+            notificationSent = true -- Set the flag to true after sending the notification
         end
         wait(1)
     end
 end
-
 		local femboyelliot = MiscTab:CreateToggle({
 			Name = "Femboy Elliot Skin",
 			Callback = function()
