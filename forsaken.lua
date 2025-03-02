af768dd48f843ee11f7eaa73bdac6048817f3297
Automatic Key Update <pm@sussy.dev>
2025-03-02 09:52:25 +0000

    Auto Update Key

--- forsaken.lua
+++ forsaken.lua
@@ -232,7 +232,7 @@ local function fartsakenLoad()
 			FileName = "fartsakenKey",
 			SaveKey = true,
 			GrabKeyFromSite = false,
-			Key = { "mogged" },
+			Key = { "wherethehuzzat" },
 		},
 	})
 
